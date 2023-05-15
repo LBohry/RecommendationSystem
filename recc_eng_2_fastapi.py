4051e3dcc27c6645254538b34a2d50d093940064
LBohry <baha.eddine.abd@gmail.com>
2023-05-15 15:00:55 +0100

    integrated quantia similarities into the api

--- recc_eng_2_fastapi.py
+++ recc_eng_2_fastapi.py
@@ -8,26 +8,43 @@ app = FastAPI()
 
 
 # Assuming that we're using AWS S3 Buckets which have versioning enabled 
+# s3_client = boto3.client('s3')
+# bucket_name = 'your-bucket-name'
 
-##response = s3_client.list_objects_v2(Bucket=bucket_name)
-##objects = sorted(response['Contents'], key=lambda x: x['LastModified'], reverse=True)
-##
-##latest_object_key = None
-##for obj in objects:
-##    if re.match(r'courses_similarity_df_v.*\.csv', obj['Key']):
-##        latest_object_key = obj['Key']
-##        break
-##if latest_object_key is None:
-##    raise Exception("No similarity dataframe found in the S3 bucket")
-##
-##
-##
+
+# Function to get the latest object key with a specific pattern
+#def get_latest_object_key(pattern):
+#   response = s3_client.list_objects_v2(Bucket=bucket_name)
+#   objects = sorted(response['Contents'], key=lambda x: x['LastModified'], reverse=True)
+#   latest_object_key = None
+#   for obj in objects:
+#       if re.match(pattern, obj['Key']):
+#           latest_object_key = obj['Key']
+#           break
+#   if latest_object_key is None:
+#       raise Exception(f"No object found in the S3 bucket with pattern: {pattern}")
+#   return latest_object_key
+
+
+### Download the latest similarity_df
+##similarity_pattern = r'courses_similarity_df_v.*\.csv'
+##latest_similarity_key = get_latest_object_key(similarity_pattern)
 ##with open("courses_similarity_df_latest.csv", "wb") as download_file:
-##    s3_client.download_fileobj(bucket_name, latest_object_key, download_file)
-##
-##
-##
+##    s3_client.download_fileobj(bucket_name, latest_similarity_key, download_file)
 ##similarity_df = pd.read_csv("courses_similarity_df_latest.csv")
+##
+
+
+# Download the latest diverse_default_recommendations_list
+##recommendations_pattern = r'diverse_default_recommendations_dict_spectral_v.*\.pkl'
+
+##latest_recommendations_key = get_latest_object_key(recommendations_pattern)
+
+##with open("diverse_default_recommendations_dict_spectral_latest.pkl", "wb") as download_file:
+##    s3_client.download_fileobj(bucket_name, latest_recommendations_key, download_file)
+
+##with open("diverse_default_recommendations_dict_spectral_latest.pkl", "rb") as file:
+##    diverse_recommendations = pickle.load(file)
 
 
 
@@ -37,13 +54,16 @@ file_name = 'courses_similarity_df.pkl'
 similarity_df = pd.read_pickle(file_name)
 distance_matrix = 1 - similarity_df
 
+quantia_similarities_df = pd.read_pickle('quantia_similarities_df.pkl')
+quantia_dataset_cleaned = pd.read_pickle('quantia_dataset_cleaned.pkl')
+
 courses_data = pd.read_pickle('preprocessed_df_withclusters.pkl')
 with open("diverse_default_recommendations_dict_spectral.pkl", "rb") as f:
     loaded_course_dict = pickle.load(f)
 
 
 
-@app.get('/recommend-courses/{course_id}')
+@app.get('/recommend-courses/IT/{course_id}')
 async def get_recommendations(course_id: str):
 
     target_course_similarity = similarity_df.loc[course_id]
@@ -52,6 +72,17 @@ async def get_recommendations(course_id: str):
 
     return {'recommended_courses': top_20_similar_courses}
 
+@app.get('/recommend-courses/MED/{course_id}')
+async def get_recommendations(course_id: int):
+
+    target_course_similarity = quantia_similarities_df.loc[course_id]
+    top_3_similar_courses = target_course_similarity.sort_values(ascending=False).head(4)[1:]
+    top_3_similar_courses_indices = top_3_similar_courses.index
+    top_3_course_headlines = quantia_dataset_cleaned.loc[top_3_similar_courses_indices, 'HEADLINE']
+
+    similarity_dict = {headline: similarity for headline, similarity in zip(top_3_course_headlines, top_3_similar_courses)}
+
+    return {'recommended_courses': similarity_dict}
 
 @app.get('/recommend-default-courses')
 async def get_diverse_recommendations():
